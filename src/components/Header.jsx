ff5e8c24cf93fd64edc68c01229dce2e66d950b0
Evgenii-Buslaev <eugenebus144@gmail.com>
2022-09-12 11:23:26 +0500

    Created Header component

---
+++ src/components/Header.jsx
@@ -0,0 +1,12 @@
+import styles from "./Header.module.css";
+
+function Header() {
+  return (
+    <>
+      <div className={styles.header}></div>
+      <hr></hr>
+    </>
+  );
+}
+
+export default Header;
