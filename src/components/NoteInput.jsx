6e4a019b137d453b8abb11b7d27c8fe072965738
Evgenii-Buslaev <eugenebus144@gmail.com>
2022-09-13 13:05:33 +0500

    Made NoteInput be contolled, added state

--- src/components/NoteInput.jsx
+++ src/components/NoteInput.jsx
@@ -3,10 +3,10 @@ import closeIcon from "../icons/note-input/close.png";
 
 import classes from "../css/components/NoteInput.module.css";
 
-function NoteInput() {
+function NoteInput({ change }) {
   return (
     <div className={classes.noteInputCont}>
-      <input type="sumbit" placeholder="Заметка..."></input>
+      <input type="sumbit" placeholder="Заметка..." onChange={change}></input>
       <button>
         <img src={confirmIcon} alt="confirm-button-icon" />
       </button>
--- src/components/NotesList.jsx
+++ src/components/NotesList.jsx
@@ -3,19 +3,33 @@ import NoteInput from "./NoteInput";
 /* import BackgroundOfEmptyList from "./BackgroundOfEmptyList"; */
 import Note from "./Note";
 
+import { useState } from "react";
+
 /* import notes from "../icons/menu/storage.png";
 import collection from "../icons//menu/collection.png";
 import recycleBin from "../icons//menu/recycle-bin.png"; */
 
 function NotesList() {
+  /* const [notes, setNotes] = useState([]); */
+  const [inputNote, setInputNote] = useState("");
+
+  function createNote() {}
+  function changeInput(e) {
+    setInputNote(e.target.value);
+  }
   return (
     <div className={styles.notesCont}>
-      <NoteInput />
+      <NoteInput change={changeInput} />
       <Note text="Тестовая заметка" />
-      {/* <BackgroundOfEmptyList>
-        <img src={notes} alt="notes-empty-background" />
-        <h3>Здесь будут храниться Ваши заметки.</h3>
-      </BackgroundOfEmptyList> */}
+
+      {/* {notes ? (
+        notes.map((elem) => <Note text={elem.text} />)
+      ) : (
+        <BackgroundOfEmptyList>
+          <img src={notes} alt="notes-empty-background" />
+          <h3>Здесь будут храниться Ваши заметки.</h3>
+        </BackgroundOfEmptyList>
+      )} */}
     </div>
   );
 }
