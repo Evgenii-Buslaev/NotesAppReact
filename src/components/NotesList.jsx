6f7985a59e28f51f55e69e40c67d901674c2f782
Evgenii-Buslaev <eugenebus144@gmail.com>
2022-09-13 13:25:52 +0500

    Added id from uuid library

--- src/components/NotesList.jsx
+++ src/components/NotesList.jsx
@@ -1,10 +1,15 @@
+import { useState } from "react";
+import { v4 as uuidv4 } from "uuid";
+
+// styles
 import styles from "../css/components/NotesList.module.css";
+
+// components
 import NoteInput from "./NoteInput";
 import BackgroundOfEmptyList from "./BackgroundOfEmptyList";
 import Note from "./Note";
 
-import { useState } from "react";
-
+// icons
 import notes from "../icons/menu/storage.png";
 /* import collection from "../icons//menu/collection.png";
 import recycleBin from "../icons//menu/recycle-bin.png"; */
@@ -14,8 +19,9 @@ function NotesList() {
   const [inputNote, setInputNote] = useState("");
 
   function createNote() {
-    setNotesList([...notesList, inputNote]);
+    setNotesList([...notesList, { text: inputNote, id: uuidv4() }]);
     setInputNote("");
+    console.log(notesList);
   }
 
   function changeInput(e) {
@@ -26,7 +32,9 @@ function NotesList() {
     <div className={styles.notesCont}>
       <NoteInput change={changeInput} add={createNote} />
       {notesList.length ? (
-        notesList.map((elem) => <Note text={elem} />)
+        notesList.map((elem) => (
+          <Note text={elem.text} id={elem.id} key={elem.id} />
+        ))
       ) : (
         <BackgroundOfEmptyList>
           <img src={notes} alt="notes-empty-background" />
