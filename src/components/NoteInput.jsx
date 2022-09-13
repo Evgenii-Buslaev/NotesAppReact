25b363378dc5076ca4653fec667bb116856a68a6
Evgenii-Buslaev <eugenebus144@gmail.com>
2022-09-13 13:16:32 +0500

    Added creating a note function

--- src/components/NoteInput.jsx
+++ src/components/NoteInput.jsx
@@ -3,11 +3,16 @@ import closeIcon from "../icons/note-input/close.png";
 
 import classes from "../css/components/NoteInput.module.css";
 
-function NoteInput({ change }) {
+function NoteInput({ change, add }) {
   return (
     <div className={classes.noteInputCont}>
-      <input type="sumbit" placeholder="Заметка..." onChange={change}></input>
-      <button>
+      <input
+        type="sumbit"
+        placeholder="Заметка..."
+        onChange={change}
+        onSubmit={add}
+      ></input>
+      <button onClick={add}>
         <img src={confirmIcon} alt="confirm-button-icon" />
       </button>
       <button>
--- src/components/NotesList.jsx
+++ src/components/NotesList.jsx
@@ -1,35 +1,38 @@
 import styles from "../css/components/NotesList.module.css";
 import NoteInput from "./NoteInput";
-/* import BackgroundOfEmptyList from "./BackgroundOfEmptyList"; */
+import BackgroundOfEmptyList from "./BackgroundOfEmptyList";
 import Note from "./Note";
 
 import { useState } from "react";
 
-/* import notes from "../icons/menu/storage.png";
-import collection from "../icons//menu/collection.png";
+import notes from "../icons/menu/storage.png";
+/* import collection from "../icons//menu/collection.png";
 import recycleBin from "../icons//menu/recycle-bin.png"; */
 
 function NotesList() {
-  /* const [notes, setNotes] = useState([]); */
+  const [notesList, setNotesList] = useState([]);
   const [inputNote, setInputNote] = useState("");
 
-  function createNote() {}
+  function createNote() {
+    setNotesList([...notesList, inputNote]);
+    setInputNote("");
+  }
+
   function changeInput(e) {
     setInputNote(e.target.value);
   }
+
   return (
     <div className={styles.notesCont}>
-      <NoteInput change={changeInput} />
-      <Note text="Тестовая заметка" />
-
-      {/* {notes ? (
-        notes.map((elem) => <Note text={elem.text} />)
+      <NoteInput change={changeInput} add={createNote} />
+      {notesList.length ? (
+        notesList.map((elem) => <Note text={elem} />)
       ) : (
         <BackgroundOfEmptyList>
           <img src={notes} alt="notes-empty-background" />
           <h3>Здесь будут храниться Ваши заметки.</h3>
         </BackgroundOfEmptyList>
-      )} */}
+      )}
     </div>
   );
 }
