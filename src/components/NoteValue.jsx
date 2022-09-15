6601c06ae9370e455000354a1a0f7b7f8cd8874d
Evgenii-Buslaev <eugenebus144@gmail.com>
2022-09-15 16:51:47 +0500

    Imported autosized textarea into note component

--- src/components/NoteValue.jsx
+++ src/components/NoteValue.jsx
@@ -1,18 +1,20 @@
 import { useState } from "react";
-
+import TextareaAutosize from "react-textarea-autosize";
 import styles from "../css/components/Note.module.css";
 
 function NoteValue({ text, edit, id }) {
   const [noteText, setNoteText] = useState(text);
 
+  function changeTextareaValue(e) {
+    setNoteText(e.target.value);
+    edit(id, e.target.value);
+  }
+
   return (
-    <textarea
+    <TextareaAutosize
       className={styles.noteText}
       value={noteText}
-      onChange={(e) => {
-        setNoteText(e.target.value);
-        edit(id, e.target.value);
-      }}
+      onChange={changeTextareaValue}
     />
   );
 }
