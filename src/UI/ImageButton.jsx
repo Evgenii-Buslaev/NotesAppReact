2ccc9f298c96361f9f141d6923526ad449d8941b
Evgenii-Buslaev <eugenebus144@gmail.com>
2022-09-20 15:19:17 +0500

    Added pin button for Note

--- src/UI/ImageButton.jsx
+++ src/UI/ImageButton.jsx
@@ -2,13 +2,12 @@ import styles from "../css/components/NoteInput.module.css";
 
 function ImageButton({ onClickHandler, title, imageSrc, imageAlt }) {
   return (
-    <button onClick={onClickHandler} title={title}>
-      <img
-        style={{ width: "2rem", marginTop: "1rem" }}
-        className={styles.formButtons}
-        src={imageSrc}
-        alt={imageAlt}
-      />
+    <button
+      className={styles.formButtons}
+      onClick={onClickHandler}
+      title={title}
+    >
+      <img src={imageSrc} alt={imageAlt} />
     </button>
   );
 }
--- src/components/NoteValue.jsx
+++ src/components/NoteValue.jsx
@@ -3,10 +3,13 @@ import TextareaAutosize from "react-textarea-autosize";
 
 import styles from "../css/components/Note.module.css";
 import { AppContext } from "../handlers/context";
+import ImageButton from "../UI/ImageButton";
 
+import pin from "../icons/note/pin.png";
+import unpin from "../icons/note/pinned.png";
 
 function NoteValue({ text, id }) {
-  const { storage, setStorage, edit } = useContext(AppContext)
+  const { storage, setStorage, edit } = useContext(AppContext);
   const [noteText, setNoteText] = useState(text);
 
   function changeTextareaValue(e) {
@@ -15,11 +18,26 @@ function NoteValue({ text, id }) {
   }
 
   return (
-    <TextareaAutosize
-      className={styles.noteText}
-      value={noteText}
-      onChange={changeTextareaValue}
-    />
+    <div
+      style={{
+        width: "90%",
+        display: "flex",
+        justifyContent: "space-between",
+        alignItems: "flex-start",
+      }}
+    >
+      <TextareaAutosize
+        className={styles.noteText}
+        value={noteText}
+        onChange={changeTextareaValue}
+      />
+      <ImageButton
+        title="Закрепить"
+        imageSrc={pin}
+        imageAlt="pin note"
+        onClickHandler={() => console.log("pin")}
+      />
+    </div>
   );
 }
 
